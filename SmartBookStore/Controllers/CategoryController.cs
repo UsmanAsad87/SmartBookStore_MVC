a32a4df41f5a17d39c4b3d72a13e229edf3c9bf9
usmanasad87 <usmanasad0324@gmail.com>
2024-06-24 01:38:01 +0500

    Upto Lecture 15 - Category CRUD Operation

---
+++ SmartBookStore/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartBookStore.Data;
+using SmartBookStore.Models;
+
+namespace SmartBookStore.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
