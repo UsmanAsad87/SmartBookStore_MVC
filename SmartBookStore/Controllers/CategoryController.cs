1a8291a3347529bb45fccad5766deb837f6f1d1f
usmanasad87 <usmanasad0324@gmail.com>
2024-06-30 18:15:07 +0500

    Category crud operatin done

--- SmartBookStore/Controllers/CategoryController.cs
+++ SmartBookStore/Controllers/CategoryController.cs
@@ -16,5 +16,93 @@ namespace SmartBookStore.Controllers
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name== obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("", "The Display Order cannot match the Name");
+            }
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category Created successfully";
+                return RedirectToAction("Index");
+
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id==0) {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id); // first Method work on primary key
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(c => c.Id == id);
+            //Category? categoryFromDb2= _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("", "The Display Order cannot match the Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category Updated successfully";
+                return RedirectToAction("Index");
+
+            }
+            return View();
+        }
+
+
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id); // first Method work on primary key
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.Categories.Find(id); 
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category Deleted successfully";
+
+            return RedirectToAction("Index");
+        }
     }
 }
